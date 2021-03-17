f53f75e8340afdf040187508159870ab5f6f59b3
sandy <sandytiassasmita48@gmail.com>
2021-03-17 13:51:57 +0700

    to do list using context api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DataProvider } from "./component/DataProvider";
+import Footer from "./component/Footer";
+import FormInput from "./component/FormInput";
+import List from "./component/List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <div className="App">
+        <h1>To Do List</h1>
+        <FormInput />
+        <List />
+        <Footer />
+      </div>
+    </DataProvider>
   );
 }
 
